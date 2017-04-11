4df6e7142b5a0ff12fea9048d0e4202463b29c8e
smaharj1 <smaharj1@ramapo.edu>
2017-04-11 15:16:00 -0400

    Design for submission of assignments done

--- ekaH-Windows/Profiles/UserControllers/Student/StudentAssignmentUC.Designer.cs
+++ ekaH-Windows/Profiles/UserControllers/Student/StudentAssignmentUC.Designer.cs
@@ -37,7 +37,18 @@
             this.projectName = new MetroFramework.Controls.MetroLabel();
             this.weightTextBox = new MetroFramework.Controls.MetroLabel();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.materialDivider1 = new MaterialSkin.Controls.MaterialDivider();
+            this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel6 = new MetroFramework.Controls.MetroLabel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.metroLabel7 = new MetroFramework.Controls.MetroLabel();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // metroLabel4
@@ -45,7 +56,7 @@
             this.metroLabel4.AutoSize = true;
             this.metroLabel4.FontSize = MetroFramework.MetroLabelSize.Tall;
             this.metroLabel4.FontWeight = MetroFramework.MetroLabelWeight.Bold;
-            this.metroLabel4.Location = new System.Drawing.Point(3, 449);
+            this.metroLabel4.Location = new System.Drawing.Point(0, 527);
             this.metroLabel4.Name = "metroLabel4";
             this.metroLabel4.Size = new System.Drawing.Size(116, 25);
             this.metroLabel4.TabIndex = 24;
@@ -68,7 +79,7 @@
             this.assignmentRTB.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.assignmentRTB.Location = new System.Drawing.Point(0, 57);
             this.assignmentRTB.Name = "assignmentRTB";
-            this.assignmentRTB.Size = new System.Drawing.Size(864, 374);
+            this.assignmentRTB.Size = new System.Drawing.Size(864, 443);
             this.assignmentRTB.TabIndex = 15;
             this.assignmentRTB.Text = "";
             // 
@@ -136,10 +147,109 @@
             this.panel1.Size = new System.Drawing.Size(262, 25);
             this.panel1.TabIndex = 26;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::ekaH_Windows.Properties.Resources.submitIcon;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 576);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 27;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(171)))), ((int)(((byte)(227)))));
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.metroLabel6);
+            this.panel2.Controls.Add(this.metroLabel5);
+            this.panel2.Location = new System.Drawing.Point(150, 576);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(717, 128);
+            this.panel2.TabIndex = 28;
+            // 
+            // materialDivider1
+            // 
+            this.materialDivider1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialDivider1.Depth = 0;
+            this.materialDivider1.Location = new System.Drawing.Point(134, 576);
+            this.materialDivider1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialDivider1.Name = "materialDivider1";
+            this.materialDivider1.Size = new System.Drawing.Size(10, 128);
+            this.materialDivider1.TabIndex = 29;
+            this.materialDivider1.Text = "materialDivider1";
+            // 
+            // metroLabel5
+            // 
+            this.metroLabel5.AutoSize = true;
+            this.metroLabel5.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel5.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.metroLabel5.ForeColor = System.Drawing.Color.White;
+            this.metroLabel5.Location = new System.Drawing.Point(3, 0);
+            this.metroLabel5.Name = "metroLabel5";
+            this.metroLabel5.Size = new System.Drawing.Size(69, 25);
+            this.metroLabel5.TabIndex = 0;
+            this.metroLabel5.Text = "Status: ";
+            this.metroLabel5.UseCustomBackColor = true;
+            this.metroLabel5.UseCustomForeColor = true;
+            // 
+            // metroLabel6
+            // 
+            this.metroLabel6.AutoSize = true;
+            this.metroLabel6.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel6.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.metroLabel6.ForeColor = System.Drawing.Color.White;
+            this.metroLabel6.Location = new System.Drawing.Point(3, 25);
+            this.metroLabel6.Name = "metroLabel6";
+            this.metroLabel6.Size = new System.Drawing.Size(99, 25);
+            this.metroLabel6.TabIndex = 0;
+            this.metroLabel6.Text = "Submitted:";
+            this.metroLabel6.UseCustomBackColor = true;
+            this.metroLabel6.UseCustomForeColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(155)))), ((int)(((byte)(227)))));
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.metroLabel7);
+            this.panel3.Location = new System.Drawing.Point(514, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(203, 128);
+            this.panel3.TabIndex = 30;
+            // 
+            // metroLabel7
+            // 
+            this.metroLabel7.AutoSize = true;
+            this.metroLabel7.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel7.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.metroLabel7.ForeColor = System.Drawing.Color.White;
+            this.metroLabel7.Location = new System.Drawing.Point(0, 0);
+            this.metroLabel7.Name = "metroLabel7";
+            this.metroLabel7.Size = new System.Drawing.Size(59, 25);
+            this.metroLabel7.TabIndex = 0;
+            this.metroLabel7.Text = "Grade";
+            this.metroLabel7.UseCustomBackColor = true;
+            this.metroLabel7.UseCustomForeColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 65);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TBD";
+            // 
             // StudentAssignmentUC
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.materialDivider1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.weightTextBox);
             this.Controls.Add(this.metroLabel4);
@@ -152,6 +262,11 @@
             this.Size = new System.Drawing.Size(867, 750);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -168,5 +283,13 @@
         private MetroFramework.Controls.MetroLabel projectName;
         private MetroFramework.Controls.MetroLabel weightTextBox;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private MaterialSkin.Controls.MaterialDivider materialDivider1;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private MetroFramework.Controls.MetroLabel metroLabel6;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private MetroFramework.Controls.MetroLabel metroLabel7;
     }
 }
--- ekaH-Windows/Properties/Resources.Designer.cs
+++ ekaH-Windows/Properties/Resources.Designer.cs
@@ -159,5 +159,15 @@ namespace ekaH_Windows.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap submitIcon {
+            get {
+                object obj = ResourceManager.GetObject("submitIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
